b13ab9f74998ef3b04939ec2f4360ee533fa872d
Maksim <i@mpozdnjakov.ru>
2023-02-10 13:35:55 +0300

    Lecture_001

--- Lectures/Lecture_001.java
+++ Lectures/Lecture_001.java
@@ -2,6 +2,41 @@ package Lectures;
 
 public class Lecture_001 {
     public static void main(String[] args) {
-        System.out.println("Heloo World");
+        //System.out.println("Heloo World");
+        //String s = "2";
+        //System.out.println(s);
+
+        // побитовые сдвиги
+        //int a = 8;
+        // в двоичной форме будет 1000
+        // можно сделать так -> a = a << 1;
+        // или так:
+        //System.out.println(a << 1);
+
+        // побитовые операции & | ^
+        // int a = 5;
+        // int b = 2;
+        // System.out.println(a | b);
+        // boolean a = true;
+        // boolean b = true;
+        // System.out.println(a & b);
+
+        // одномерные массивы
+        // int[] arr = new int[10]; // 10 элементов
+        // System.out.println(arr.length);
+        // arr = new int[] {1, 2, 3, 4, 5}; // 5 элементов
+        // System.out.println(arr.length);
+
+        // многомерные массивы
+        // int[] arr[] = new int[3][5];
+        // for (int[] line: arr) {
+            // for (int item: line) {
+                // System.out.printf("%d", item);
+            // }
+            // System.out.println();
+        // }
+
+
+
     }
 }
