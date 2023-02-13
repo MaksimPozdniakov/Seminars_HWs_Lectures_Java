054a4152c4d4eafa1fde81481dc9106d51e09e1e
Maksim <p.m.pavlovich@ya.ru>
2023-02-13 17:50:17 +0300

    HW_01

--- HomeWorks/HW1/Task_02.java
+++ HomeWorks/HW1/Task_02.java
@@ -4,6 +4,17 @@ package HomeWorks.HW1;
 
 public class Task_02 {
     public static void main(String[] args) {
-
+        int max_value = 1000;
+        boolean f = true;
+        for (int i = 2; i <= max_value; i++){
+            for (int j = 2; j < i; j++){
+                if (i % j == 0){
+                    f = false;
+                    break;
+                }
+            }
+            if (f) System.out.println(i);
+            else f = true;
+        }
     }
 }
