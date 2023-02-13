d473944fc7c5549d70c97fd4a91987e693fc219a
Maksim <p.m.pavlovich@ya.ru>
2023-02-13 19:01:43 +0300

    HW_01

--- HomeWorks/HW1/Task_04.java
+++ HomeWorks/HW1/Task_04.java
@@ -1,36 +1,39 @@
-//package HomeWorks.HW1;
-import java.util.Arrays;
+package HomeWorks.HW1;
+
 // Задача №4. Задано уравнение вида q + w = e, q, w, e >= 0. Некоторые цифры
 //могут быть заменены знаком вопроса, например 2? + ?5 = 69. Требуется
 //восстановить выражение до верного равенства. Предложить хотя бы
 //одно решение или сообщить, что его нет.
 
-//public class Task_04 {
-//    public static void main(String[] args) {
-//        String str = "2? + 51 = ?9";
-//        String [] expression = str.split(" ");
-//        String q = expression[0];
-//        String w = expression[2];
-//        String z = expression[expression.length - 1];
-//
-//        boolean b = true;
-//        for (int i = 0; i < 10; i++) {
-//            int qNum = Integer.parseInt(q.replace("?", Integer.toString(i)));
-//            for (int j =0; j < 10; j++) {
-//                int wNum = Integer.parseInt(w.replace("?", Integer.toString(j)));
-//                for (int k = 0; k < 10; k++) {
-//                    int zNum = Integer.parseInt(z.replace("?", Integer.toString(k)));
-//                    if (qNum + wNum == zNum) {
-//                        System.out.printf("Решение уравнения %s => %d + %d = %d\n",
-//                                str, qNum, wNum, zNum);
-//                        b = false;
-//                        break;
-//                    }
-//                }
-//                if (b == false) break;
-//            }
-//            if (b == false) break;
-//        }
-//        if (b) System.out.println("Решений нет!");
-//    }
-//}
+public class Task_04 {
+    public static void main(String[] args) {
+        String str = "2? + ?5 = 69";
+        String [] expression = str.split(" ");
+        String q = expression[0];
+        String w = expression[2];
+        String z = expression[expression.length - 1];
+
+        boolean b = true;
+        for (int i = 0; i < 10; i++) {
+            int q_num = Integer.parseInt(q.replace("?", Integer.toString(i)));
+            for (int j =0; j < 10; j++) {
+                int w_num = Integer.parseInt(w.replace("?", Integer.toString(j)));
+                for (int k = 0; k < 10; k++) {
+                    int z_num = Integer.parseInt(z.replace("?", Integer.toString(k)));
+                    if (q_num + w_num == z_num) {
+                        System.out.printf("Решение есть: %d + %d = %d\n", q_num, w_num, z_num);
+                        b = false;
+                        break;
+                    }
+                }
+                if (!b){
+                    break;
+                }
+            }
+            if (!b){
+                break;
+            }
+        }
+        if (b) System.out.println("Решений нет!");
+    }
+}
