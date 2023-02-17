a3c5635a3a6adf3a8511ef940e5a9770ee9ca677
Maksim <p.m.pavlovich@ya.ru>
2023-02-17 10:00:58 +0300

    HW_01

--- Seminars/Seminar_002.java
+++ Seminars/Seminar_002.java
@@ -1,4 +1,4 @@
-package Seminars;
+//package Seminars;
 
 // Задача №1. Дано четное число N (>0) и символы c1 и c2.
 // Написать метод, который вернет строку длины N, которая состоит из чередующихся символов c1 и c2, начиная с c1.
@@ -127,50 +127,50 @@ package Seminars;
 //    3) Обработайте ошибки с помощью try-catch конструкции. В случае возникновения исключения, оно должно
 //    записаться в лог-файл.
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.logging.FileHandler;
-import java.util.logging.Logger;
-import java.util.logging.FileHandler.encoding=UTF-8
-
-public class Seminar_002 {
-    public static void main(String[] args) throws IOException {
-        Logger logger = Logger.getLogger(Seminar_002.class.getName());
-        FileHandler fh = new FileHandler("log.txt");
-        logger.addHandler(fh);
-
-
-        String startDir = System.getProperty("user.dir");
-        String[] data = getDirArr(startDir);
-        writeToFile(data);
-    }
-
-    public static String[] getDirArr(String dirPath) {
-        try {
-//            String pathProject = System.getProperty("user.dir");
-            File dir = new File(dirPath);
-            return dir.list();
-        }catch (Exception e) {
-            System.out.println("Тут какое-то исключение");
-            return new String[0];
-        }
-    }
-    public static void writeToFile(String[] data){
-        String result = String.join(", ", data);
-        try{
-            String pathProdject = System.getProperty("user.dir");
-            String pathFile = pathProdject.concat("\\file3.txt");
-            File file = new File(pathFile);
-
-            FileWriter fileWriter = new FileWriter(file, true);
-            fileWriter.write(result);
-            fileWriter.flush();
-            fileWriter.close();
-        }
-        catch (Exception e){
-            System.out.println("Вот исключение.");
-            e.printStackTrace();
-        }
-    }
-}
+//import java.io.File;
+//import java.io.FileWriter;
+//import java.io.IOException;
+//import java.util.logging.FileHandler;
+//import java.util.logging.Logger;
+//import java.util.logging.FileHandler.encoding=UTF-8
+//
+//public class Seminar_002 {
+//    public static void main(String[] args) throws IOException {
+//        Logger logger = Logger.getLogger(Seminar_002.class.getName());
+//        FileHandler fh = new FileHandler("log.txt");
+//        logger.addHandler(fh);
+//
+//
+//        String startDir = System.getProperty("user.dir");
+//        String[] data = getDirArr(startDir);
+//        writeToFile(data);
+//    }
+//
+//    public static String[] getDirArr(String dirPath) {
+//        try {
+////            String pathProject = System.getProperty("user.dir");
+//            File dir = new File(dirPath);
+//            return dir.list();
+//        }catch (Exception e) {
+//            System.out.println("Тут какое-то исключение");
+//            return new String[0];
+//        }
+//    }
+//    public static void writeToFile(String[] data){
+//        String result = String.join(", ", data);
+//        try{
+//            String pathProdject = System.getProperty("user.dir");
+//            String pathFile = pathProdject.concat("\\file3.txt");
+//            File file = new File(pathFile);
+//
+//            FileWriter fileWriter = new FileWriter(file, true);
+//            fileWriter.write(result);
+//            fileWriter.flush();
+//            fileWriter.close();
+//        }
+//        catch (Exception e){
+//            System.out.println("Вот исключение.");
+//            e.printStackTrace();
+//        }
+//    }
+//}
