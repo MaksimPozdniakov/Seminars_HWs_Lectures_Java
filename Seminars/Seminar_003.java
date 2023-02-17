0775c6db3d857b40fac043721894db5fba663a34
Maksim <p.m.pavlovich@ya.ru>
2023-02-17 10:40:20 +0300

    Sem_3

--- Seminars/Seminar_003.java
+++ Seminars/Seminar_003.java
@@ -1,4 +1,38 @@
 package Seminars;
 
+// Задача №1. Даны следующие строки, cравнить их с помощью == и метода equals() класса Object
+// String s1 = "hello";
+// String s2 = "hello";
+// String s3 = s1;
+// String s4 = "h" + "e" + "l" + "l" + "o";
+// String s5 = new String("hello");
+// String s6 = new String(new char[]{'h', 'e', 'l', 'l', 'o'});
+
 public class Seminar_003 {
+    public static void main(String[] args) {
+
+    }
 }
+
+// Задача №2. Заполнить список десятью случайными числами.
+// Отсортировать список методом sort() и вывести его на экран.
+
+//public class Seminar_003 {
+//    public static void main(String[] args) {
+//
+//    }
+//}
+
+
+//public class Seminar_003 {
+//    public static void main(String[] args) {
+//
+//    }
+//}
+
+
+//public class Seminar_003 {
+//    public static void main(String[] args) {
+//
+//    }
+//}
