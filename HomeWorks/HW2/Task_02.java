379cc54d9c55d6f10114d6e89ee1de77ff8c1fe0
Maksim <i@mpozdnjakov.ru>
2023-02-17 22:36:50 +0300

    HW_02

--- HomeWorks/HW2/Task_02.java
+++ HomeWorks/HW2/Task_02.java
@@ -1,4 +1,30 @@
 package HomeWorks.HW2;
 
+// Реализуйте алгоритм сортировки пузырьком числового массива, результат после каждой итерации запишите в лог-файл.
+
+import java.util.Arrays;
+import java.util.Random;
+
 public class Task_02 {
+    public static void main(String[] args) {
+        int[] digit_arr = new int[10];
+        Random random = new Random();
+        for (int i = 0; i < digit_arr.length; i++) {
+            digit_arr[i] = random.nextInt(5, 25);
+        }
+        System.out.println(Arrays.toString(digit_arr));
+
+        for (int i = 0; i < digit_arr.length; i++) {
+            for (int j = i+1; j < digit_arr.length; j++) {
+                if (digit_arr[j] < digit_arr[i]) {
+                    int temp = digit_arr[j];
+                    digit_arr[j] = digit_arr[i];
+                    digit_arr[i] = temp;
+                }
+            }
+            System.out.println(Arrays.toString(digit_arr));
+        }
+//        System.out.println(Arrays.toString(digit_arr));
+
+    }
 }
--- HomeWorks/HW2/Task_04.java
+++ HomeWorks/HW2/Task_04.java
@@ -1,4 +1,6 @@
 package HomeWorks.HW2;
 
+// К калькулятору из предыдущего дз добавить логирование.
+
 public class Task_04 {
 }
