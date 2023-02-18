2d1c7b1555b496d2db4b9471d3b572141688c66c
Maksim <p.m.pavlovich@ya.ru>
2023-02-18 23:50:04 +0300

    HW_02

--- HomeWorks/HW2/Task_03.java
+++ HomeWorks/HW2/Task_03.java
@@ -10,8 +10,37 @@ package HomeWorks.HW2;
 // Студент Петрова получил 4 по предмету Информатика.
 // Студент Краснов получил 5 по предмету Физика.
 
-public class Task_03 {
-    public static void main(String[] args)  {
 
+import java.util.List;
+import java.util.Map;
+import java.nio.file.Paths;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.nio.file.Files;
+import java.nio.charset.StandardCharsets;
+import com.fasterxml.jackson.core.type.TypeReference;
+
+
+public class Task_03 {
+    public static void main(String[] args) throws Exception {
+        List<Map<String, Object>> jsonString = Read_and_Pars("C:\\Users\\PMPav\\Desktop\\Second block of study\\" +
+                "Seminars\\Seminars_HWs_Lectures_Java\\HomeWorks\\HW2\\FileToTask3.json");
+        Show_result(jsonString);
+    }
+
+    public static List<Map<String, Object>> Read_and_Pars(String path) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString = Files.readString(Paths.get(path), StandardCharsets.UTF_8);
+        return mapper.readValue(jsonString, new TypeReference<>() {
+        });
+    }
+
+    public static void Show_result(List<Map<String, Object>> jsonString) {
+        StringBuilder str = new StringBuilder();
+        for (Map<String, Object> entry : jsonString) {
+            str.append(String.format("Студент %s получил %s по предмету %s.\n", entry.get("фамилия"),
+                    entry.get("оценка"), entry.get("предмет")));
+        }
+        System.out.println(str);
     }
 }
+
