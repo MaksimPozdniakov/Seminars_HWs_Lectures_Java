8348919cecedb6be81a93b85693bc93a6c71eb7b
Maksim <i@mpozdnjakov.ru>
2023-03-02 16:36:45 +0300

    L_06

---
+++ Lectures/Lecture_006_Set/Task_03.java
@@ -0,0 +1,12 @@
+package Lectures.Lecture_006_Set;
+
+import java.util.*;
+public class Task_03 {
+    public static void main(String[] args) {
+        var a = new LinkedHashSet<>(Arrays.asList(1,7,2,3,6,4,5));
+        var b = new LinkedHashSet<>(Arrays.asList(13,3,17,7,2,11,5));
+        a.add(28);
+        System.out.println(a); // [1, 7, 2, 3, 6, 4, 5, 28]
+        System.out.println(b); // [13, 3, 17, 7, 2, 11, 5]
+    }
+}
