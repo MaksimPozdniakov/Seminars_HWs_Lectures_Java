716b80db7a418edd140449935252fd67b4cc81ad
Maksim <i@mpozdnjakov.ru>
2023-03-22 20:27:50 +0300

    HW5

--- OOP_ALL/HomeWork/HW_05/Model/Notebook.java
+++ OOP_ALL/HomeWork/HW_05/Model/Notebook.java
@@ -10,8 +10,8 @@ public class Notebook {
 
     public Notebook() throws IOException {
         this.jsonList = new ArrayList<>();
-        this.jsonList = ReadJson.readAndPars("C:\\Users\\PMPav\\OneDrive\\" +
-                "Рабочий стол\\Study\\Java\\OOP_ALL\\HomeWork\\HW_5\\DB\\OurDB.json");
+        this.jsonList = ReadJson.readAndPars("C:\\Users\\PMPav\\OneDrive\\Рабочий стол\\Study\\" +
+                "Seminars_HWs_Lectures_Java\\OOP_ALL\\HomeWork\\HW_05\\DB\\OurDB.json");
     }
 
 //    public void addNewNote()
