b0323be2b282f410e9870f80832347a2440c0381
Maksim <i@mpozdnjakov.ru>
2023-03-23 15:06:15 +0300

    HW5

--- OOP_ALL/HomeWork/HW_05/Program.java
+++ OOP_ALL/HomeWork/HW_05/Program.java
@@ -1,18 +1,12 @@
 package OOP_ALL.HomeWork.HW_05;
 
-import OOP_ALL.HomeWork.HW_05.model.categories.ImportantThings;
-import OOP_ALL.HomeWork.HW_05.presenter.Notebook;
+import OOP_ALL.HomeWork.HW_05.view.ConsoleUI;
 
 import java.io.IOException;
 
 public class Program {
     public static void main(String[] args) throws IOException {
-        Notebook notes = new Notebook();
-        notes.addNote(new ImportantThings("21.02.2023", "23.02.2023", "Помыть собаку",
-                "Жена просила помыть собаку"));
-
-
-        notes.write();
-        notes.read();
+        ConsoleUI start = new ConsoleUI();
+        start.showMenu();
     }
 }
--- OOP_ALL/HomeWork/HW_05/view/ConsoleUI.java
+++ OOP_ALL/HomeWork/HW_05/view/ConsoleUI.java
@@ -1,4 +1,41 @@
 package OOP_ALL.HomeWork.HW_05.view;
 
+import OOP_ALL.HomeWork.HW_05.presenter.Notebook;
+
+import java.io.IOException;
+import java.util.Scanner;
+
 public class ConsoleUI {
+
+    protected Scanner scanner;// = new Scanner(System.in);
+    Notebook notes = new Notebook();
+
+    public ConsoleUI() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void showMenu() throws IOException {
+        String textMenu = """
+                1. Показать все записи
+                2. Добавить новую запись
+                3. Удалить запись
+                4. Выйти из программы
+                """;
+        System.out.println(textMenu);
+        System.out.print("\tСделайте выбор: ");
+        int ourChoice = scanner.nextInt();
+        menu(ourChoice);
+    }
+
+    public void menu(int ourChoice) throws IOException {
+        switch (ourChoice){
+            case 1:
+                notes.read();
+            case 4:
+                System.exit(0);
+
+        }
+    }
+
+
 }
