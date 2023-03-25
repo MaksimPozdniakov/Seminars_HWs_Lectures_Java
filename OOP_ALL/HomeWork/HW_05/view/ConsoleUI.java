5f228af9d0dff718055802743c694ad122c23f02
Maksim <p.m.pavlovich@ya.ru>
2023-03-25 14:11:29 +0300

    HW5

--- OOP_ALL/HomeWork/HW_05/view/ConsoleUI.java
+++ OOP_ALL/HomeWork/HW_05/view/ConsoleUI.java
@@ -31,22 +31,23 @@ public class ConsoleUI {
             System.out.print("\tСделайте выбор: ");
             int ourChoice = scanner.nextInt();
             switch (ourChoice) {
-                case 1 -> notes.readFile();
+                case 1 -> {
+                    notes.readFile();
+                    System.out.println("Блакнот открыт!");
+                }
                 case 2 -> notes.showAllNotes();
                 case 3 -> addNewNote();
                 case 4 -> removeNote();
                 case 5 -> notes.write();
                 case 6 -> System.exit(0);
             }
-
         }
     }
 
 
     public void addNewNote() throws IOException {
-//        boolean flag = true;
-        int flag = 1;
-        while (flag == 1) {
+        boolean flag = true;
+        while (true) {
             String textMenu = """
                 1. Добавить важную задачу
                 2. Добавить неважную задачу
@@ -57,31 +58,48 @@ public class ConsoleUI {
             System.out.print("\tСделайте выбор: ");
             int ourChoice = scanner.nextInt();
             scanner.nextLine();
-//            boolean flag = true;
-//            String flag = "+";
 
-            System.out.print("Укажите дату добавления: ");
-            String date = scanner.nextLine();
-            System.out.print("Укажите название задачи: ");
-            String nameThing = scanner.nextLine();
-            System.out.print("Напишите описание задачи: ");
-            String text = scanner.nextLine();
+//            System.out.print("Укажите дату добавления: ");
+//            String date = scanner.nextLine();
+//            System.out.print("Укажите название задачи: ");
+//            String nameThing = scanner.nextLine();
+//            System.out.print("Напишите описание задачи: ");
+//            String text = scanner.nextLine();
 
-            switch (ourChoice){
-                case 1:
+            switch (ourChoice) {
+                case 1 -> {
+                    System.out.print("Укажите дату добавления: ");
+                    String date1 = scanner.nextLine();
                     System.out.print("Укажите дедлайн: ");
-                    String deadline = scanner.nextLine();
-                    notes.addNote(new ImportantThings(date,deadline,nameThing,text));
-                    break;
-                case 2:
-                    notes.addNote(new UnimportantThings(date,nameThing,text));
-                    break;
-                case 3:
-                    notes.addNote(new ProductList(date,nameThing,text));
-                    break;
-                case 4:
-                    flag = 0;
-                    break;
+                    String deadline1 = scanner.nextLine();
+                    System.out.print("Укажите название задачи: ");
+                    String nameThing1 = scanner.nextLine();
+                    System.out.print("Напишите описание задачи: ");
+                    String text1 = scanner.nextLine();
+                    notes.addNote(new ImportantThings(date1, deadline1, nameThing1, text1));
+                }
+                case 2 -> {
+                    System.out.print("Укажите дату добавления: ");
+                    String date2 = scanner.nextLine();
+                    System.out.print("Укажите название задачи: ");
+                    String nameThing2 = scanner.nextLine();
+                    System.out.print("Напишите описание задачи: ");
+                    String text2 = scanner.nextLine();
+                    notes.addNote(new UnimportantThings(date2, nameThing2, text2));
+                }
+                case 3 -> {
+                    System.out.print("Укажите дату добавления: ");
+                    String date3 = scanner.nextLine();
+                    System.out.print("Укажите название задачи: ");
+                    String nameThing3 = scanner.nextLine();
+                    System.out.print("Напишите описание задачи: ");
+                    String text3 = scanner.nextLine();
+                    notes.addNote(new ProductList(date3, nameThing3, text3));
+                }
+                case 4 -> flag = false;
+            }
+            if (!flag){
+                break;
             }
         }
         notes.updateDb();
