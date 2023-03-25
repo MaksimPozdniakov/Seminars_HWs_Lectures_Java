5b7c81a7e631ac2e64f59dd8782682b48e75593a
Maksim <p.m.pavlovich@ya.ru>
2023-03-25 15:58:31 +0300

    HW5

--- OOP_ALL/HomeWork/HW_04/DynamicArray.java
+++ OOP_ALL/HomeWork/HW_04/DynamicArray.java
@@ -1,7 +1,5 @@
 package OOP_ALL.HomeWork.HW_04;
 
-import OOP_ALL.Seminars.Seminar_01.Introduction.A;
-
 import java.util.Arrays;
 
 public class DynamicArray<T extends Comparable<T>> {
@@ -45,9 +43,117 @@ public class DynamicArray<T extends Comparable<T>> {
         array = Arrays.copyOf(array,length);
     }
 
-    public void print(){
-        System.out.println(Arrays.toString(this.array));
-        System.out.println(this.length);
+    public T  minEl(){
+       T min = array[0];
+        for (int i = 0; i < length; i++) {
+            if (this.array[i].compareTo(min) < 0){
+                min = array[i];
+            }
+        }
+        return min;
     }
 
+    public T  maxEl(){
+        T max = array[0];
+        for (int i = 0; i < length; i++) {
+            if (this.array[i].compareTo(max) > 0){
+                max = array[i];
+            }
+        }
+        return max;
+    }
+
+    public int sumEl(){
+        int sum = 0;
+        for (int i = 0; i < length; i++) {
+            if (array[i] instanceof Number) {
+                sum += ((Number) array[i]).intValue();
+            }
+        }
+        return sum;
+    }
+
+    public Object productElements(){
+        if (length == 0) throw new IllegalArgumentException("Переданный массив пустой");
+        if (array[0] instanceof Number) {
+            Number product = 1;
+            for (int i = 0; i < length; i++) {
+                product = product.doubleValue() * ((Number) array[i]).doubleValue();
+            }
+            return product;
+        }
+        return null;
+    }
+
+//    Поиск индекса заданного элемента в массиве, если такого элемента в массиве нет то возвращать -1
+    public int indexOf(T value) {
+        for (int i = 0; i < length; i++) {
+            if (array[i].equals(value)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public boolean contains(T value) {
+        return indexOf(value) != -1;
+    }
+//    Получение элемента массива по индексу
+    public T getDig(int index){
+        return array[index];
+    }
+//    Задание значения элементу массива с заданным индексом
+    public T changeDig(int index, T num){
+//        array[index] = array[num];
+        return array[index] = num;
+    }
+
+//    Пузырьковая сортировка
+    public void bubbleSorting(){
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = 0; j < length - 1; j++) {
+                if (this.array[j].compareTo(this.array[j + 1]) > 0){
+                    T temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+
+                }
+            }
+        }
+    }
+
+//Сортировка простыми вставками
+    public void insertionSort() {
+        for (int i = 1; i < length; i++) {
+            T temp = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j].compareTo(temp) > 0) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = temp;
+        }
+    }
+
+//Сортировка простым выбором
+    public void selectionSort() {
+        for (int i = 0; i < length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < length; j++) {
+                if (array[j].compareTo(array[minIndex]) < 0) {
+                    minIndex = j;
+                }
+            }
+            T temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+    }
+
+
+    public void print(){
+        System.out.println(Arrays.toString(this.array));
+//        System.out.println(this.length);
+    }
 }
--- OOP_ALL/HomeWork/HW_04/Program.java
+++ OOP_ALL/HomeWork/HW_04/Program.java
@@ -6,27 +6,62 @@ public class Program {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Integer[] ourArray = {10,25,11,32,34,23,59,29,49,11,222,999,11};
+
         DynamicArray<Integer> dynArray = new DynamicArray<>(ourArray);
 
         System.out.print("Наш изначальный массив: ");
         dynArray.print();
 
-        System.out.print("Укажите новый эелемент массива: ");
-        int newElem = scanner.nextInt();
-        dynArray.addInArr(newElem);
-        System.out.print("Наш массив после добавления нового элемента: ");
-        dynArray.print();
+//        System.out.print("Укажите новый эелемент массива: ");
+//        int newElem = scanner.nextInt();
+//        dynArray.addInArr(newElem);
+//        System.out.print("Наш массив после добавления нового элемента: ");
+//        dynArray.print();
+//
+//        System.out.print("Укажите индекс элемента, который необходимо удалить: ");
+//        int removeElem = scanner.nextInt();
+//        dynArray.removeEl(removeElem);
+//        System.out.print("Наш массив после удаления элемента: ");
+//        dynArray.print();
+//
+//        System.out.print("Укажите каких значений не должно быть в нашем массиве: ");
+//        int removeSpecDig = scanner.nextInt();
+//        dynArray.removeAllSpecDig(removeSpecDig);
+//        System.out.print("Наш массив после удаления элементов: ");
+//        dynArray.print();
 
-        System.out.print("Укажите индекс элемента, который необходимо удалить: ");
-        int removeElem = scanner.nextInt();
-        dynArray.removeEl(removeElem);
-        System.out.print("Наш массив после удаления элемента: ");
-        dynArray.print();
+//        int minDig = dynArray.minEl();
+//        System.out.printf("Минимальный элемент нашего массива: %d", minDig);
+//        System.out.println();
+//
+//        int maxDig = dynArray.maxEl();
+//        System.out.printf("Максимальный элемент нашего массива: %d", maxDig);
+//        System.out.println();
 
-        System.out.print("Укажите каких значений не должно быть в нашем массиве: ");
-        int removeSpecDig = scanner.nextInt();
-        dynArray.removeAllSpecDig(removeSpecDig);
-        System.out.print("Наш массив после удаления элементов: ");
-        dynArray.print();
+//        int sumDig = dynArray.sumEl();
+//        System.out.printf("Сумма элементов нашего массива: %d", sumDig);
+
+//        System.out.println("Произведение элементов массива: " + dynArray.productElements());
+
+//        System.out.println(dynArray.indexOf(23));
+//        System.out.println(dynArray.contains(23));
+
+//        System.out.println(dynArray.getDig(5));
+
+//        dynArray.bubbleSorting();
+//        System.out.print("Массив после сортировки пузырьком: ");
+//        dynArray.print();
+
+//        dynArray.insertionSort();
+//        System.out.print("Массив после сортировки вставками: ");
+//        dynArray.print();
+//
+//        dynArray.insertionSort();
+//        System.out.print("Массив после сортировки выбором: ");
+//        dynArray.print();
+
+//        System.out.println(dynArray.changeDig(0,123));
+//        dynArray.print();
+
     }
 }
